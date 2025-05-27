4d83778cfe16bdb9bd7f591200e55690f1e204df
Alexr6667 <aray500@gmail.com>
2025-05-27 22:47:45 +0100

    Book directory taken from CLI arguments

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from stats import get_book_text, get_character_count
+import sys
 
 def main():
-    book = "frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_dir = sys.argv[1]
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at books/{book}...")
+    print(f"Analyzing book found at {book_dir}...")
     print("---------- Word Count ----------")
-    get_book_text(f"./books/{book}")
+    get_book_text(book_dir)
     print("------- Character Count -------")
-    get_character_count(f"./books/{book}")
+    get_character_count(book_dir)
 
 main()
